c4492e49d8f46e53b1cb49a43e2f085cd715b4ed
emlikates04-maker <emlikates04@gmail.com>
2026-04-19 15:17:56 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -69,4 +69,22 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server çalışıyor");
 });const path = require("path");
-     const PORT = process.env.PORT || 3000;                                                                                           
+     const PORT = process.env.PORT || 3000;      
+                                                                                          const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server çalışıyor 🚀");
+});
+
+app.post("/idea", (req, res) => {
+  res.json({ idea: "Çalışıyor" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server çalışıyor");
+});
