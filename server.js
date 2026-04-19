54acd59071de8a65716855381fb3e435c07a3d0c
emlikates04-maker <emlikates04@gmail.com>
2026-04-19 16:08:00 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -115,3 +115,10 @@ const server = http.createServer((req, res) => {
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT);
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("OK 🚀");
+});
+
+server.listen(process.env.PORT || 3000);
