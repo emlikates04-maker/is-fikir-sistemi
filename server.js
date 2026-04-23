1e98ea208351e891a176a6784a5c3f720861ce90
emlikates04-maker <emlikates04@gmail.com>
2026-04-23 12:06:44 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const path = require("path");
 
 const app = express();
 
-// 🔥 EN KRİTİK SATIR
+S
 app.use(express.static(path.join(__dirname, "public")));
 
-// Ana route garantiye al
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Port ayarı (Render için şart)
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
